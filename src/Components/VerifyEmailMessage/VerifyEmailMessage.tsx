0112f54dc3885118df24400e70dc78f86431ef65
lmaxxx <leshchenko.maks@gmail.com>
2021-09-26 10:39:47 +0300

    Add favicon and verify page UI

--- src/Components/VerifyEmailMessage/VerifyEmailMessage.tsx
+++ src/Components/VerifyEmailMessage/VerifyEmailMessage.tsx
@@ -6,12 +6,14 @@ import Button from '@mui/material/Button';
 import { updateEmail, sendEmailVerification } from "firebase/auth";
 import Alert from '@mui/material/Alert';
 import Snackbar from '@mui/material/Snackbar';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const VerifyEmailMessage = () => {
   const [showInput, setShowInput] = useState<boolean>(false)
   const [newEmail, setNewEmail] = useState<string>('')
   const [errorMessage, setErrorMessage] = useState<string>('')
   const [showErrorMessage, setShowErrorMessage] = useState<boolean>(false)
+  const [emailError, setEmailError] = useState<boolean>(false)
   const {email} = auth.currentUser as {email: string}
 
   const changeUserEmail = async (e: FormEvent<HTMLFormElement>) => {
@@ -27,22 +29,42 @@ const VerifyEmailMessage = () => {
     }
   }
 
+  const validate = () => {
+    const validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if(validEmail.test(newEmail)) {
+      setEmailError(false)
+    } else {
+      setEmailError(true)
+    }
+  }
+
   return (
     <div className={classes.VerifyEmailMessage}>
-      <h1>Verify your email</h1>
-      <p onClick={() => auth.signOut()}>We just send your verify link to {email}</p>
-      <p onClick={() => setShowInput(prev => !prev)} >Set other email</p>
-      {
-        showInput ? 
-        <form onSubmit={(e: FormEvent<HTMLFormElement>) => changeUserEmail(e)} >
-          <TextField value={newEmail} onChange={(e: ChangeEvent<HTMLInputElement>) => setNewEmail(e.target.value)} label="New Email" variant="outlined" />
-          <Button type="submit" size="large" variant="outlined">Submit</Button>
-        </form>
-        :
-        null
-      }
+      <div className={classes.VerifyEmailMessageBlock}>
+        <MailOutlineIcon className={classes.VerifyEmailMessageIcon} />
+        <h2 className={classes.VerifyEmailMessageTitle}>Verify your email</h2>
+        <p onClick={() => auth.signOut()}>We just send your verify link to</p>
+        <strong className={classes.VerifyEmailMessageEmail}>{email}</strong>
+        <p onClick={() => setShowInput(prev => !prev)} className={classes.VerifyEmailMessageToggle} >Set other email</p>
+        {
+          showInput ? 
+          <form className={classes.VerifyEmailMessageForm} onSubmit={(e: FormEvent<HTMLFormElement>) => changeUserEmail(e)} >
+            <TextField 
+              onBlur={validate} 
+              value={newEmail} 
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setNewEmail(e.target.value)} 
+              label="New Email" 
+              variant="outlined" 
+              error={emailError}
+            />
+            <Button disabled={!newEmail || !!emailError} type="submit" size="large" variant="outlined">Submit</Button>
+          </form>
+          :
+          null
+        }
+      </div>
       <Snackbar open={showErrorMessage} autoHideDuration={4000} onClose={() => setShowErrorMessage(false)}>
-        <Alert variant="filled" severity="error">{errorMessage}</Alert>
+          <Alert variant="filled" severity="error">{errorMessage}</Alert>
       </Snackbar>
     </div>
   )
