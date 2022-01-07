792d5e58fae20bad8dc4d62d24b38a78c5724e3d
lmaxxx <leshchenko.maksym10@gmail.com>
2022-01-07 20:32:36 +0200

    7 step of mobile adaptation

--- src/Components/CodeCompiler/CodeCompiler.tsx
+++ src/Components/CodeCompiler/CodeCompiler.tsx
@@ -3,14 +3,18 @@ import {useSelector} from "react-redux";
 import {StoreType} from "../../Store";
 import CodeEditor from "../CodeEditor/CodeEditor";
 import CodeOutput from "../CodeOutput/CodeOutput";
+import useMedia from 'use-media'
+import NavBar from "../NavBar/NavBar";
 
 const CodeCompiler = () => {
   const theme = useSelector((state: StoreType) => state.app.currentUser.theme)
+  const isTablet = useMedia({maxWidth: "850px"})
 
   return (
     <div className={classes["CodeCompiler" + theme]}>
       <CodeEditor />
       <CodeOutput />
+      {isTablet && <NavBar />}
     </div>
   )
 }
